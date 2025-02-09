96572fa748fb1691d062848ae6438e7ae051fbfa
cwelchik <cwelchik@ncsu.edu>
2025-02-09 18:04:15 -0500

    added comment to task1.sh

--- tasks/task1.sh
+++ tasks/task1.sh
@@ -2,4 +2,6 @@
 sh infinite.sh
 
 # finds process id of infinite.sh and kills it
+# '[s]h' has the [] around 's' so that grep doesn't return its own pid after searching for 'sh infinite.sh'
+# 'aux' after ps shows more columns in ps table
 kill -9 $(ps aux | grep '[s]h infinite.sh' | gawk '{print $2}')
